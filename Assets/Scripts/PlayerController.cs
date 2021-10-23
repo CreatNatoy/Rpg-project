d0e5e22dd90abe22b717e40e8660ebd009afac73
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-10-23 06:44:20 +0300

    - Added simple fighting system;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,59 @@
 using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control
 {    
     public class PlayerController : MonoBehaviour
     {
     private Mover _mover;
+    private Fighter _fighter;
 
     private void Start()
     {
         _mover = GetComponent<Mover>();
+        _fighter = GetComponent<Fighter>();
     }
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        InteractWithCombat();
+        InteractWithMovement();
+    }
+
+    private void InteractWithCombat()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+        foreach (RaycastHit hit in hits)
+        {
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            if(target == null) continue;
+
+            if(Input.GetMouseButtonDown(0))
+                _fighter.Attack(target);
+        }
+    }
+
+    private void InteractWithMovement()
+    {
+        if (Input.GetMouseButton(0))
             MoveToCursor();
     }
 
     private void MoveToCursor()
-    {
-        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-        RaycastHit _hit;
-        bool hasHit = Physics.Raycast(_ray, out _hit);
-        if (hasHit)
         {
-            _mover.MoveTo(_hit.point);
+            RaycastHit _hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out _hit);
+            if (hasHit)
+            {
+                _mover.MoveTo(_hit.point);
+            }
         }
-    }
 
-
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
     }
 }
