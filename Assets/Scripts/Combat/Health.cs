8b78b42b03ea23f173f64952ddbab546550319c1
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-10-25 14:31:11 +0300

    - Trigger Death Animations;  - Any State Animations;

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,30 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] private float _health = 100f;
+        [SerializeField] private float _healthPoints = 100f;
+        private Animator _animator;
+        private bool isDead = false;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
 
         public void TakeDamage(float damage)
         {
-            _health = Mathf.Max(_health - damage, 0);
-            print(_health);
+            _healthPoints = Mathf.Max(_healthPoints - damage, 0);
+            if(_healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if(isDead) return;
+            isDead = true;
+            _animator.SetTrigger("Die");
         }
     }
 }
