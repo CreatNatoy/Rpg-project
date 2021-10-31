e7c65652b15cde9922891e0159a47ac4fa5f2fe4
CreatNatoy <76531899+CreatNatoy@users.noreply.github.com>
2021-10-31 11:05:09 +0100

    Cinematics

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+using RPG.Core;
+
+namespace RPG.Control
+{    
+    public class PlayerController : MonoBehaviour
+    {
+    private Mover _mover;
+    private Fighter _fighter;
+    private Health _health; 
+
+    private void Start()
+    {
+        _health = GetComponent<Health>(); 
+        _mover = GetComponent<Mover>();
+        _fighter = GetComponent<Fighter>();
+    }
+
+    private void Update()
+    {
+           if (_health.IsDead()) return; 
+
+           if(InteractWithCombat()) return;
+           if(InteractWithMovement()) return; 
+    }
+
+    private bool InteractWithCombat() 
+    {
+        RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+        foreach (RaycastHit hit in hits)
+        {
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+            if(!_fighter.CanAttack(target.gameObject))
+            {
+                continue;
+            }
+
+            if(Input.GetMouseButton(0))
+                _fighter.Attack(target.gameObject);
+            
+            return true;    
+        }
+        return false;
+    }
+
+    private bool InteractWithMovement()
+        {
+            RaycastHit _hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out _hit);
+            if (hasHit)
+            {
+                if (Input.GetMouseButton(0))
+                    _mover.StartMoveAction(_hit.point, 1f);
+                return true;
+            }
+            return false;
+        }
+
+    private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -9,25 +9,29 @@ namespace RPG.Movement
 public class Mover : MonoBehaviour, IAction
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private float _maxSpeed = 6f; 
 
     private NavMeshAgent _navMeshAgent;
     private ActionScheduler _action;
+        private Health _health; 
 
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>(); 
         _action = GetComponent<ActionScheduler>();
+        _health = GetComponent<Health>(); 
     }
 
     private void Update()
-    {   
+    {
+        _navMeshAgent.enabled = !_health.IsDead(); 
         UpdateAnimator();
     }
 
-    public void StartMoveAction(Vector3 destination)
+    public void StartMoveAction(Vector3 destination, float speedFraction)
     {
         _action.StartAction(this);
-        MoveTo(destination);
+        MoveTo(destination, speedFraction);
     }
 
     public void Cancel()
@@ -35,16 +39,16 @@ public class Mover : MonoBehaviour, IAction
         _navMeshAgent.isStopped = true;
     }
 
-    public void MoveTo(Vector3 destination)
+    public void MoveTo(Vector3 destination, float speedFraction)
     {
         _navMeshAgent.destination = destination;
+            _navMeshAgent.speed = _maxSpeed * Mathf.Clamp01(speedFraction); 
         _navMeshAgent.isStopped = false;
     }
 
     private void UpdateAnimator()
     {
-        Vector3 velocity = _navMeshAgent.velocity;  //�������� �������� 
-        //transform.InverseTransformDirection()  ��������������� �� �������� ������������ � ��������� ������������  
+        Vector3 velocity = _navMeshAgent.velocity;
         Vector3 localVelocity = transform.InverseTransformDirection(velocity); 
         float speed = localVelocity.z;
         GetComponent<Animator>().SetFloat("Blend", speed); 
