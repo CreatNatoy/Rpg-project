92dccedffc8f5834da49378fbaf76bc686ddb36f
CreatNatoy <76531899+CreatNatoy@users.noreply.github.com>
2021-12-03 10:35:09 +0100

    Add HealthBar

---
+++ Assets/Scripts/Core/HealthBar.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Health _healthComponent = null;
+        [SerializeField] private RectTransform _foreground = null;
+        [SerializeField] private Canvas _rootCanvas = null;
+
+        /*      private void Update()
+              {
+                  if(_healthComponent.GetHealthBar() <= 0 )
+                  {
+                      _rootCanvas.enabled = false;
+                      return; 
+                  }
+                  else
+                  _foreground.localScale = new Vector3(_healthComponent.GetHealthBar(), 1, 1); 
+              }
+        */
+        private void OnEnable()
+        {
+            _healthComponent.UpdateHealthBar += UpdateHealthBar;
+        }
+
+        private void OnDisable()
+        {
+            _healthComponent.UpdateHealthBar -= UpdateHealthBar;
+        }
+
+        public void UpdateHealthBar(float zero)
+        {
+            if (_healthComponent.GetHealthBar() <= 0)
+            {
+                _rootCanvas.enabled = false;
+                return;
+            }
+            else
+                _foreground.localScale = new Vector3(_healthComponent.GetHealthBar(), 1, 1);
+        }
+
+    }
+}
