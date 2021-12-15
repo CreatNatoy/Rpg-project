f0bc7b9dd285669b1abba0ec565b1ebbdfe7080e
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-12-15 23:56:05 +0200

    - Added ExperienceDisplay;

--- Assets/Scripts/Resources/Experience.cs
+++ Assets/Scripts/Resources/Experience.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using RPG.Saving;
+using System;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0;
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public float GetPoints()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
